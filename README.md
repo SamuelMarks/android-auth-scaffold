c3df47922afaf1c4d3200132f9b018efa949db87
Samuel Marks <samuelmarks@gmail.com>
2017-12-04 14:53:43 +1100

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+android-auth-scaffold
+=====================
+
+Analgous to [ng-material-scaffold](https://github.com/SamuelMarks/ng-material-scaffold) [for web]; but for Android!
---
+++ app/src/main/java/io/offscale/samuel/android_auth_scaffold/api/contact/ContactsClient.java
@@ -0,0 +1,45 @@
+package io.offscale.samuel.android_auth_scaffold.api.contact;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import io.offscale.samuel.android_auth_scaffold.utils.ApiClient;
+import io.offscale.samuel.android_auth_scaffold.utils.BaseApiClient;
+import io.offscale.samuel.android_auth_scaffold.utils.ErrorOrEntity;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+import static io.offscale.samuel.android_auth_scaffold.utils.GsonSingleton.getGson;
+import static io.offscale.samuel.android_auth_scaffold.utils.IMimeTypes.MEDIA_TYPE_JSON;
+
+public final class ContactsClient extends BaseApiClient {
+    private final String mApiPrefix = "/contact";
+    private final Gson mGson = getGson();
+
+    public ContactsClient(final Context context, final String accessToken) {
+        super(context, null, null, null, null, true, accessToken);
+    }
+
+    public final Request get() {
+        return new Request.Builder()
+                .url(getBaseUri() + mApiPrefix)
+                .get()
+                .build();
+    }
+
+    public final Request post(final Contact contact) {
+        return new Request.Builder()
+                .url(getBaseUri() + mApiPrefix)
+                .post(RequestBody.create(MEDIA_TYPE_JSON, mGson.toJson(contact)))
+                .build();
+    }
+
+    public final ErrorOrEntity<Contact> postSync(final Contact contact) {
+        return ApiClient.sync(getClient(), post(contact), Contact.class);
+    }
+
+    public final ErrorOrEntity<ListContacts> getSync() {
+        return ApiClient.sync(getClient(), get(), ListContacts.class);
+    }
+}
